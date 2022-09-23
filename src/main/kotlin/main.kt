0bec56c95c2a44d9ca37fc6633030497e4834229
den-rgb <den02_m@yahoo.com>
2022-09-23 12:35:05 +0100

    fixed merge conflict

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -86,7 +86,6 @@ fun listGames(){
     }
 }
 
-
 fun updateGame(games: MutableMap<String, Game>){
     for (g in gameList){
         println("-------> "+g.value.name + " " + g.key)
@@ -138,56 +137,6 @@ fun removeGame(games: MutableMap<String, Game>){
             println(" Game id or Game name doesn't exist!")
             removeGame(gameList)
         }
-=======
-    fun addGame(games: MutableMap<String, Game>){
-        println(box +"Enter game details...." )
-
-        print("Name: ")
-        val name = readLine()!!
-        if (games.containsValue(Game(name))){
-            println("This game already exists here!!")
-            return addGame(gameList)
-        }
-
-        print("Price: ")
-        val price = readLine()!!
-
-        val rnd = Random()
-        var id: Int = rnd.nextInt(10000)
-        println("Game Id: " + id)
-        println()
-        games[id.toString()] = Game(name,id,price.toFloat())
-    }
-
-    fun listGames(){
-        println("---------- Your Games --------------")
-        println()
-        for (g in gameList){
-            println("-------> "+g.value.name)
-        }
-    }
-
-    fun updateGame(games: MutableMap<String, Game>){
-        for (g in gameList){
-            println("-------> "+g.value.name + " " + g.key)
-        }
-        println(" Choose a game to update ")
-        print( "---Game Id: ")
-        val input = readLine()!!
-        if (input.toIntOrNull() != null && input.isNotEmpty()) {
-            input.toInt()
-        }
-        if (games.containsKey(input)){
-            println(" What would you like to update?? ")
-            println( " ---> 1: Name")
-            println( " ---> 2: Price")
-            val choice = readLine()!!
-            if (choice.toIntOrNull() != null && choice.isNotEmpty()) {
-                when (choice.toInt()) {
-                    1 -> updateName(games, input.toInt())
-                    2 -> updatePrice(games,input.toInt())
-                }
-            }
     }
 }
 
