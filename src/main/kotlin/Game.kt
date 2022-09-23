3b27fdff16a876e182809e51e8d048419a482f9c
den-rgb <den02_m@yahoo.com>
2022-09-23 11:15:17 +0100

    Added search + remove

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,5 +2,6 @@ package org.setu.placemark
 
 data class Game(
     var name : String? = null,
-    var id  : Int = 0,
+    var id  : Int? = null,
+    var price : Float? = null
 )
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 package org.setu.placemark
 
 import mu.KotlinLogging
+import java.util.*
 
 private val logger = KotlinLogging.logger {}
 val red = "\u001b[31m"
@@ -14,8 +15,11 @@ fun main(args: Array<String>){
         input = menu()
         when(input) {
             1 ->  addGame(gameList)
-            2 ->  listGames(gameList)
-            3 ->  listGames(gameList)
+            2 ->  updateGame(gameList)
+            3 ->  listGames()
+            4 -> removeGame(gameList)
+            5 -> clearLib()
+            6 -> searchGame(gameList)
             -1 -> println(" Exiting App ")
             else -> println(" Invalid Option ")
         }
@@ -24,45 +28,226 @@ fun main(args: Array<String>){
     logger.info { " Shutting Down Game Library " }
 }
 
-    fun menu() : Int {
+fun menu() : Int {
 
-        var option : Int
-        var input: String? = null
+    var option : Int
+    var input: String? = null
 
-        println(red + "  Welcome to the Game Library" + "\n"  )
-        println(box  + "          Main Menu")
-        println("-------------------------------")
-        println(" 1. Add a Game to your library")
-        println(" 2. Update a Game")
-        println(" 3. List All Games")
-        println("-1. Exit")
+    println(red + "  Welcome to the Game Library" + "\n"  )
+    println(box  + "          Main Menu")
+    println("-------------------------------")
+    println(" 1. Add a Game to your library")
+    println(" 2. Update Game Info")
+    println(" 3. List All Games")
+    println(" 4. Remove Game")
+    println(" 5. Clear library")
+    println(" 6. Search")
+    println("-1. Exit")
+    println()
+    print("----->")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -1
+    return option
+}
+
+
+fun addGame(games: MutableMap<String, Game>){
+    println(box +"Enter game details...." )
+
+    print("Name: ")
+    val name = readLine()!!
+    if (games.containsValue(Game(name))){
+        println("This game already exists here!!")
+        return addGame(gameList)
+    }
+
+    print("Price: ")
+    val price = readLine()!!
+    if(price.toFloatOrNull()!=null && price.isNotEmpty()) {
+        val rnd = Random()
+        var id: Int = rnd.nextInt(10000)
+        println("Game Id: " + id)
         println()
-        print("----->")
-        input = readLine()!!
-        option = if (input.toIntOrNull() != null && !input.isEmpty())
-            input.toInt()
-        else
-            -1
-        return option
+        games[id.toString()] = Game(name, id, price.toFloat())
+    }else{
+        println(" Invalid price ")
+        addGame(gameList)
     }
+}
 
+fun listGames(){
+    println("---------- Your Games --------------")
+    println()
+    for (g in gameList){
+        println("-------> "+g.value.name  +" --- $" + g.value.price)
+    }
+}
 
-    fun addGame(games: MutableMap<String, Game>){
-        println(box +"Enter game details...." )
-        print("Id: ")
-        val id = readLine()!!
-        if (games.keys.contains(id)){
-            println("Oh Oh maybe try a different game id?")
-            return addGame(gameList)
+fun updateGame(games: MutableMap<String, Game>){
+    for (g in gameList){
+        println("-------> "+g.value.name + " " + g.key)
+    }
+    println(" Choose a game to update ")
+    print( "---Game Id: ")
+    val input = readLine()!!
+    if (input.toIntOrNull() != null && input.isNotEmpty()) {
+        input.toInt()
+    }
+    if (games.containsKey(input)){
+        println(" What would you like to update?? ")
+        println( " ---> 1: Name")
+        println( " ---> 2: Price")
+        println( " ---> 3: Back")
+        val choice = readLine()!!
+        if (choice.toIntOrNull() != null && choice.isNotEmpty()) {
+            when (choice.toInt()) {
+                1 -> updateName(games, input.toInt())
+                2 -> updatePrice(games,input.toInt())
+                3 -> menu()
+            }
         }
-        print("Name: ")
-        val name = readLine()!!
-        games[id] = Game(name,id.toInt())
     }
 
-    fun listGames(games: MutableMap<String, Game>){
+}
 
+fun updateName(games: MutableMap<String, Game>,input : Int){
+    print( "New Game name --> ")
+    val name = readLine()!!
+    games[input.toString()] = Game(name,input,games.getValue("$input").price)
+}
+
+fun updatePrice(games: MutableMap<String, Game>,input : Int){
+    print( "New Game price--> ")
+    val price = readLine()!!
+    games[input.toString()] = Game(games.getValue("$input").name,input,price.toFloat())
+}
+
+fun removeGame(games: MutableMap<String, Game>){
+    println("Which game would you like to remove")
+    listGames()
+    var input = readLine()!!
+    print("------> ")
+    for (g in games){
+        if( g.value.name.equals(input) or (g.value.id.toString() == input)){
+            gameList.remove(g.key)
+        }else {
+            println(" Game id or Game name doesn't exist!")
+            removeGame(gameList)
+        }
     }
+}
 
+fun clearLib(){
+    gameList.clear()
+}
 
+fun searchGame(games: MutableMap<String, Game>){
+    println()
+    println(" Search by: ")
+    println("----1. ID ")
+    println("----2. Name ")
+    println("----3. Price ")
+    println("----4. Category ")
+    println("----5. Back ")
+    val choice = readLine()!!
+    for (g in games){
+        if (choice.toIntOrNull() != null && choice.isNotEmpty()) {
+            when (choice.toInt()) {
+                1 -> searchId(gameList)
+                2 -> searchName(gameList)
+                3 -> searchPrice(gameList)
+                5 -> menu()
+            }
+        }
+    }
+}
 
+fun searchName(games: MutableMap<String, Game>){
+    println()
+    println("-----Name: ")
+    val input = readLine()!!
+    var i = 0
+    var x = 0
+    for (g in games){
+        if (input.isNotEmpty()) {
+            if (g.value.name.equals(input)) {
+                println("----Name: " + g.value.name + " ----Id: " + g.value.id + " ----Price: " + g.value.price)
+                i++
+                x++
+            } else {
+                i++
+            }
+
+            if (i == games.size && x == 0) {
+                println()
+                println("--- No Matches Found ---")
+                searchGame(gameList)
+            }
+        }else {
+            println( "--- Try writing something ---")
+            searchName(gameList)
+        }
+    }
+    searchGame(gameList)
+}
+
+fun searchId(games: MutableMap<String, Game>){
+    println()
+    println(" -----Id: ")
+    val input = readLine()!!
+    var i = 0
+    var x = 0
+    for (g in games){
+        if (input.toIntOrNull() != null && input.isNotEmpty()) {
+            if (g.value.id == (input.toInt())) {
+                println("----Name: " + g.value.name + " ----Id: " + g.value.id + " ----Price: " + g.value.price)
+                i++
+                x++
+            } else {
+                i++
+            }
+
+            if (i == games.size && x == 0) {
+                println()
+                println("--- No Matches Found ---")
+                searchGame(gameList)
+            }
+        }else{
+            println( "--- Try writing a number ---")
+            searchId(gameList)
+        }
+    }
+    searchGame(gameList)
+}
+
+fun searchPrice(games: MutableMap<String, Game>){
+    println()
+    println("-----Price: ")
+    val input = readLine()!!
+    var i = 0
+    var x = 0
+    for (g in games){
+        if (input.toFloatOrNull() != null && input.isNotEmpty()) {
+            if (g.value.price == (input.toFloat())) {
+                println("----Name: " + g.value.name + " ----Id: " + g.value.id + " ----Price: " + g.value.price)
+                i++
+                x++
+            } else {
+                i++
+            }
+
+            if (i == games.size && x == 0) {
+                println()
+                println("--- No Matches Found ---")
+                searchGame(gameList)
+            }
+        }else{
+            println( "--- Try writing a number ---")
+            searchPrice(gameList)
+        }
+    }
+    searchGame(gameList)
+}
